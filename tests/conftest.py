5c646740cb6f5143bef86b1d89577aa2cbd5bf94
AmyMcCann <amy.mccann.34@gmail.com>
2023-12-15 10:42:17 +0000

    Pytest passing, h1 updated

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,8 +1,35 @@
-import pytest
+import pytest, sys,random, os, py
+from xprocess import ProcessStarter
 from app import app
+from typing import Generator
+from playwright.sync_api import (
+    BrowserContext,
+    Page,
+)
 
 @pytest.fixture
 def web_client():
     app.config['TESTING'] = True
     with app.test_client() as client:
         yield client
+
+@pytest.fixture
+def test_web_address(xprocess):
+    python_executable = sys.executable
+    app_file = py.path.local(__file__).dirpath("../app.py")
+    port = str(random.randint(4000, 4999))
+    class Starter(ProcessStarter):
+        env = {"PORT": port, "APP_ENV": "test", **os.environ}
+        pattern = "Debugger PIN"
+        args = [python_executable, app_file]
+
+    xprocess.ensure("flask_test_server", Starter)
+
+    yield f"localhost:{port}"
+
+    xprocess.getinfo("flask_test_server").terminate()
+
+@pytest.fixture
+def page(context: BrowserContext) -> Generator[Page, None, None]:
+    page = context.new_page()
+    yield page
--- tests/test_app.py
+++ tests/test_app.py
@@ -1,5 +1,10 @@
-def test_get_home(web_client):
-    pass
+from playwright.sync_api import Page, expect
+
+def test_get_home(page, test_web_address):
+    page.goto(f"http://{test_web_address}")
+    h1_tags = page.locator("h1")
+    expect(h1_tags).to_have_text("Christmas Website")
+
     # response = web_client.get("/")
     # assert response.status_code == 200
     # assert response.data.decode("utf-8") == "I am a CI-CD hero"

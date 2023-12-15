270f63f770024343d5e11a7b395404c94b408c4c
AmyMcCann <amy.mccann.34@gmail.com>
2023-12-15 14:22:49 +0000

    Created a Christmas Dinner Ideas section

--- tests/test_app.py
+++ tests/test_app.py
@@ -4,8 +4,8 @@ def test_get_home(page, test_web_address):
     page.goto(f"http://{test_web_address}")
     h1_tags = page.locator("h1")
     expect(h1_tags).to_have_text("It's almost Christmas!")
-    list_tags = page.locator("li")
-    expect(list_tags).to_have_text([
+    checklist_tags = page.locator(".t-checklist")
+    expect(checklist_tags).to_have_text([
         "Put up the decorations",
         "Write a letter to Father Christmas",
         "Buy presents",
@@ -15,6 +15,21 @@ def test_get_home(page, test_web_address):
         "Listen to Christmas Music",
         "Leave a mince pie and carrot out for Father Christmas and Ruldolph"
     ])
+    food_heading_tags = page.locator("h3")
+    expect(food_heading_tags).to_have_text([
+        'Starters:',
+        'Center Piece:',
+        'Side Dishes:'
+    ])
+    starters_tags = page.locator('.t-starters')
+    expect(starters_tags).to_have_text([
+        'Soup',
+        'Prawn Cocktail',
+        'Salmon Blinis',
+        'Pate or Parfait'
+    ]
+
+    )
 
     # response = web_client.get("/")
     # assert response.status_code == 200

ed73667b5d73767ce976e3f8dd34d4a1f1ef4c0a
AmyMcCann <amy.mccann.34@gmail.com>
2023-12-15 12:42:06 +0000

    Amended app.py port to 5000

--- app.py
+++ app.py
@@ -11,5 +11,5 @@ if __name__ == '__main__':
     app.run(
       debug=True,
       host="0.0.0.0",
-      port=int(os.environ.get('PORT', 5001)) # Listen for connections _to_ any server
+      port=int(os.environ.get('PORT', 5000)) # Listen for connections _to_ any server
     )
